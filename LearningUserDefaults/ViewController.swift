4f4304cdbc9fe572199a1071f79040d8175c2f2f
Danil Ryumin <ryumin_danil@icloud.com>
2022-01-02 00:10:09 +0300

    Black&White Theme

--- LearningUserDefaults/ViewController.swift
+++ LearningUserDefaults/ViewController.swift
@@ -15,20 +15,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var color: UIColor? {
+            guard let name = userDefaults.object(forKey: "theme") as? String,
+                    let color = Color(rawValue: name) else { return nil }
+                switch color {
+                case .black: return .black
+                case .white: return .white
+            }
+        }
+        
         switcher.isOn = userDefaults.bool(forKey: "switcher")
         imageView.image = UIImage(named: userDefaults.string(forKey: "photo")!)
+        view.backgroundColor = color
     }
     
     @IBAction func switchClick(_ sender: Any) {
         if switcher.isOn {
             userDefaults.set("gogol", forKey: "photo")
             userDefaults.set(true, forKey: "switcher")
+            userDefaults.set(Color.black.rawValue, forKey: "theme")
+            
             imageView.image = #imageLiteral(resourceName: "gogol")
+            view.backgroundColor = .black
         } else {
             userDefaults.set("turgenev", forKey: "photo")
             userDefaults.set(false, forKey: "switcher")
+            userDefaults.set(Color.white.rawValue, forKey: "theme")
+            
             imageView.image = #imageLiteral(resourceName: "turgenev")
+            view.backgroundColor = .white
         }
     }
 }
 
+enum Color: String {
+    case black
+    case white
+}
