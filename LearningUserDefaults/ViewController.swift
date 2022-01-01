89b32d73947645781c623290f173ba4c8086c150
Danil Ryumin <ryumin_danil@icloud.com>
2022-01-01 21:34:00 +0300

    Switcher&ImageView

--- LearningUserDefaults/ViewController.swift
+++ LearningUserDefaults/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var switcher: UISwitch!
+    
+    let userDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switcher.isOn = userDefaults.bool(forKey: "switcher")
+        imageView.image = UIImage(named: userDefaults.string(forKey: "photo")!)
     }
-
-
+    
+    @IBAction func switchClick(_ sender: Any) {
+        if switcher.isOn {
+            userDefaults.set("gogol", forKey: "photo")
+            userDefaults.set(true, forKey: "switcher")
+            imageView.image = #imageLiteral(resourceName: "gogol")
+        } else {
+            userDefaults.set("turgenev", forKey: "photo")
+            userDefaults.set(false, forKey: "switcher")
+            imageView.image = #imageLiteral(resourceName: "turgenev")
+        }
+    }
 }
 
